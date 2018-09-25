ad199a158ac01e870cbdac6bb0f9533053dffcbd
TheRusher28 <therusher28@gmail.com>
2018-09-25 23:50:18 +0200

    Siguiendo en el trabajo del proyecto PD: No esta terminado aún pero igual lo subo

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 """
 No esta terminado por el momento :D
 """
+from time import sleep
+
 ADD_CONTACT = 1
 REMOVE_CONTACT = 2
 SEARCH_CONTACT = 3
 EXPORT_CONTACT = 4
 EXIT = 5
+
+MENU_OPTIONS = [ADD_CONTACT, REMOVE_CONTACT, SEARCH_CONTACT, EXPORT_CONTACT]
 
 def show_menu():
     print("1. Añadir contacto")
@@ -14,21 +18,45 @@ def show_menu():
     print("4. Exportar contactos a un CSV")
     print("5. Salir")
 
+    selected_action = ''
+
+    while not selected_action.isdigit() or (selected_action.isdigit() not in MENU_OPTIONS):
+        selected_action = input('¿Qué opcion escoges?')
+
+    return int(selected_action)
+
+def add_contact(contacts):
+    print('Añadir contacto')
+    contact = [{'name': input('Nombre: ')}, {'phone': input('Telefono: ')}, {'email': input('Email: ')}]
+    contacts.append(contact)
+    print('Se ha añadido un nuevo contacto')
+    sleep(2)
+
+def remove_contact(contacts):
+    print('Eliminando')
+
+def search_contact(contacts):
+    print('Buscando')
+
+def export_contact(contacts):
+    print('Exportando')
+
 def main():
+    contacts = []
     action = show_menu()
     while action != EXIT:
         if action == ADD_CONTACT:
-            print("añadir contacto")
+            add_contact(contacts)
         elif action == REMOVE_CONTACT:
-            print("eliminar contacto")
+            remove_contact()
         elif action == SEARCH_CONTACT:
-            print("buscar contacto")
+            search_contact()
         elif action == EXPORT_CONTACT:
-            print("exportar contactos")
+            export_contact()
 
         action = show_menu()
 
-    #Salir del programa
+    print('Saliendo del programa...')
 
 if __name__ == '__main__':
-    pass
+    main()
